c1d700ab367e670bfd22728e39c365d0aae1dd51
Matthew Rohde <mrohde213@gmail.com>
2026-07-03 23:50:19 +0900

    style(name): update naming and linking in relevant files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:my_app/main.dart';
-import 'package:my_app/screens/main_shell.dart';
+import 'package:gakuji/main.dart';
+import 'package:gakuji/screens/main_shell.dart';
 
 void main() {
   setUp(() {
